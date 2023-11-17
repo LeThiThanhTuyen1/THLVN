67445805754b6e9a05ac239a2ce9ae794ca2f1cb
LeThiThanhTuyen1 <130636825+LeThiThanhTuyen1@users.noreply.github.com>
2023-11-17 09:43:54 +0700

    Update contact.php

--- contact.php
+++ contact.php
@@ -16,7 +16,7 @@
         .text {
             position: absolute;
             color: white;
-            margin: 90px 150px;
+            margin: 500px 150px;
             width: 500px;
         }
         h1 {
@@ -27,12 +27,11 @@
 <body>
     <?php include('header.php') ?>
     <div class="contact-div">
-        
         <div class="text">
             <h1>Liên hệ với chúng tôi</h1>
-            <br>Địa chỉ: Tầng 2, số nhà 541 Vũ Tông Phan, Phường Khương Đình, Quận Thanh Xuân, Thành phố Hà Nội, Việt Nam<br>
+            <br>Địa chỉ: Số nhà 541 An Dương Vương, Phường Nguyễn Văn Cừ, Thành phố Quy Nhơn, tinht Bình Địng, Việt Nam<br>
             <br>Phone: 084 000 00 00<br>
-            <br>Email: tthvoteam@gmail.com
+            <br>Email: <a class="link" style="color: #F7941D;" href="mailto:tinhoctpt@gmail.com">ttvhoteam@gmail.com.vn</a>
         </div>  
         <img src="img/nenlienhe.jpg">  
     </div>
