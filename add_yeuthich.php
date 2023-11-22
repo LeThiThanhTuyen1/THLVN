1c67868e7da9758387f76d4f40d1931dd6d8cf57
NguyenThiKieuOanh24 <oanhkieu2425@gmail.com>
2023-11-22 20:16:44 +0700

    oanh commit

--- add_yeuthich.php
+++ add_yeuthich.php
@@ -1,22 +1,34 @@
 <?php
-include 'config.php';
+    include 'config.php';
 
-if (isset($_SESSION['login_user'])) {
-    $username = $_SESSION['login_user'];
-    
-    if (isset($_POST['sid'])) {
-        $sid = $_POST['sid'];
-        
-        $query = "INSERT INTO sanyeuthich (IDSan, TenTK) VALUES ('$sid' ,'$username')";
-        
-        if ($conn->query($query)) {
-            echo "success";
+    if (isset($_SESSION['login_user'])) {
+        $username = $_SESSION['login_user'];
+
+        if (isset($_GET['id'])) {
+            $id = $_GET['id'];
+
+            $checkQuery = "SELECT * FROM sanyeuthich WHERE IDSan = '$id' AND TenTK = '$username'";
+            $checkResult = mysqli_query($conn, $checkQuery);
+
+            if (mysqli_num_rows($checkResult) > 0) {
+                echo "<script>
+                        alert('Sân bóng đã có trong danh sách yêu thích!');
+                        window.location.href='sanbong.php';
+                    </script>";
+            } else {
+                $insertQuery = "INSERT INTO sanyeuthich (IDSan, TenTK) VALUES ('$id' ,'$username')";
+
+                if (mysqli_query($conn, $insertQuery)) {
+                    echo "<script>
+                            alert('Đã thêm vào danh sách yêu thích!');
+                            window.location.href='sanbong.php';
+                        </script>";
+                } else {
+                    echo "Lỗi: " . mysqli_error($conn);
+                }
+            }
         } else {
-            echo "error: " . $conn->error;
+            echo "Thiếu tham số ID.";
         }
-        $conn->close();
-        } else {
-            echo "error: Missing sid parameter";
-        }
-    }                 
+    }
 ?>
