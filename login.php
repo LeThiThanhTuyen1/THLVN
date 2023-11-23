0a16184ae8d714a66e667b0dc2ba5489fd7fa2f2
LeThiThanhTuyen <130636825+LeThiThanhTuyen1@users.noreply.github.com>
2023-11-23 21:15:08 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="UTF-8">
-	<title>Đăng Nhập - TPT</title>
+	<title>Đăng Nhập</title>
 	<link rel="icon" type="image/x-icon" href="img/logo.ico">
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<style>
