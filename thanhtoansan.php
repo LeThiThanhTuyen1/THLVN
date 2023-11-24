aa56db1320144b8aded7c82d9fb5f72e1d99dd66
LeThiThanhTuyen <130636825+LeThiThanhTuyen1@users.noreply.github.com>
2023-11-24 22:39:26 +0700

    Update thanhtoansan.php

--- thanhtoansan.php
+++ thanhtoansan.php
@@ -40,15 +40,15 @@
 		background: #f9f9f9;
 		margin-bottom: 20px;
 	}
-        .add form input[type="text"] {
-            margin-bottom: -20px;
-        }
-        textarea {
-            height: 50px;
-            margin-bottom: -20px;
-        }
+    .add form input[type="text"] {
+        margin-bottom: -20px;
+    }
+    textarea {
+        height: 50px;
+        margin-bottom: -20px;
+    }
 	.add form input[type="submit"] {
-        margin-top: 30px;
+        margin-top: 30px; 
 		background: #125c0b;
 		color: #fff;
 		border: none;
@@ -56,6 +56,7 @@
 		padding: 10px;
 		font-size: 18px;
 		cursor: pointer;
+        margin-left: 20%;
 	}
 	
     </style>
@@ -69,44 +70,75 @@
             echo "Lỗi: Không có ID sân được chỉ định.";
             exit;
         }
-
+        if(isset($_POST['huy'])) {
+            header('location: danhsachdat.php');
+        }
         if(isset($_POST['luu'])) {
-           
-                $sql = "UPDATE datsan SET DaThanhToan = 1 WHERE MaDat = '$id'";
+            $stk = $_POST['stk'];
+            $ten = $_POST['tenTK'];
+            if(!empty($ten)) {
+                if(!empty($stk)) {
+                    $sql = "UPDATE datsan SET DaThanhToan = 1 WHERE MaDat = '$id'";
 
-                if (mysqli_query($conn, $sql)) {
+                    if (mysqli_query($conn, $sql)) {
+                        echo "<script>
+                        alert('Bạn đã thanh toán thành công!');
+                        window.location = 'danhsachdat.php';
+                        </script>";
+                    } else {
+                        echo "Lỗi: " . mysqli_error($conn);
+                    }
+                }
+                else {
                     echo "<script>
-                    alert('Bạn đã thanh toán thành công!');
-                    window.location = 'danhsachdat.php';
-                    </script>";
-                } else {
-                    echo "Lỗi: " . mysqli_error($conn);
+							alert('Vui lòng nhập số tài khoản!');
+							window.location.back();
+							</script>";
                 }
             }
+            else {
+                echo "<script>
+							alert('Vui lòng nhập tên chủ tài khoản!');
+							window.location.back();
+							</script>";
+            }
+        }
 
-        $sql = "SELECT ThanhTien FROM datsan WHERE MaDat = $id";
+        $sql = "SELECT * FROM datsan WHERE MaDat = $id";
         $result = mysqli_query($conn, $sql);
-
+        
+        
         if (mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_assoc($result);
+            
         } else {
             echo "Lỗi: Không tìm thấy thông tin đặt sân";
             exit;
         }
+        $i = $row['IDSan'];
+        $sql1 = "SELECT TenSan FROM sanbong where ID = $i";
+        $result1 = mysqli_query($conn, $sql1);
+        $row1 = mysqli_fetch_assoc($result1);
     ?>
    <div class="add">
-        <h1>Thanh toán tiền đặt sân</h1>
-        <form method="post" action="" enctype="multipart/form-data">
+        <h1>Thông tin thanh toán</h1>
+        <form method="post" action="">
+            <label for="tenKhachHang">Mã đặt:</label>
+            <input type="text" name="madat" value="<?= $id ?>" disabled="true"><br>
+
+            <label for="tenKhachHang">Tên Sân:</label>
+            <input type="text" name="tensan"  value="<?= $row1['TenSan'] ?>" disabled="true" required><br>
+
             <label for="tenKhachHang">Tên chủ tài khoản:</label>
-            <input type="text" name="tenTK" id="tenKhachHang" value="" required><br>
+            <input type="text" name="tenTK" id="tenKhachHang" value="" ><br>
 
             <label for="email">Số tài khoản:</label>
-            <input type="text" name="number" id="email" value="" required><br>
+            <input type="text" name="stk" id="stk" value="" ><br>
 
             <label for="soDT">Số tiền phải thanh toán:</label>
-            <input type="text" name="soDT" id="soDT" value="<?= $row['ThanhTien'] ?>" required><br>
+            <input type="text" name="tien" id="tien" disabled="true" value="<?= $row['ThanhTien'] ?>" required><br>
 
-            <input type="submit" name="luu" value="Thanh toán">
+            <span><input type="submit" name="luu" value="Thanh toán"><input type="submit" name="huy" value="Hủy"></span>
         </form>
     </div>
 </body>
